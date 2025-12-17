b4f4dcc71173f465f811c45dcb2a1042e021d407
060618zx-oss <060618zx@gmail.com>
2025-12-17 14:57:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'ai-paint-v1';
-const assets = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const assets = ['./', './index.html', './manifest.json', './icon-192.jpg', './icon-512.jpg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
@@ -7,4 +7,5 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
 });
